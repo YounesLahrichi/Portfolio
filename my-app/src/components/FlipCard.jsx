15ad90adf11ce45cf99bcda0ed8bb50b9a47b719
YounesLahrichi <Lahrichi.ys@gmail.com>
2023-04-15 16:48:49 -0700

    Fixed card functionality and added styling

--- my-app/src/components/FlipCard.jsx
+++ my-app/src/components/FlipCard.jsx
@@ -17,11 +17,16 @@ function FlipCard(props) {
                     onClick={() =>{
                         setShowFront((v) => !v)
                     }}>
-                    <div className="card-back">back</div>
-                    <div className="card-front">
-                        <img src={dotaLogo} className="flip_Card_ImageV"/>
-                        <h2 className='headingTest'>Hello</h2>
+                    <div className="card-back">
+                        <h2 className="card_back_heading">{props.backTitle}</h2>
+                        <h3 className="card_back_role">{props.role}</h3>
+                        <h4 className="card_back_timeline">{props.timeline}</h4>
+                        <p className="card_back_description">{props.description}</p>
 
+                    </div>
+                    <div className="card-front">
+                        <img src={props.backgroundFront} className="flip_Card_ImageV"/>
+                        <h2 className="card_front_heading">{props.frontTitle}</h2>
                     </div>
                 </div>
             </CSSTransition>
